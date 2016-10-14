c036f6ae177b7fa69cc074c96b26e11fb15103de
XueMeili <1604512498@qq.com>
2016-10-14 22:57:26 +0800

    添加log输出的工具类，并在mainactivity中测试

--- app/src/main/java/com/example/lenovobyeoz/fulicenter/activity/MainActivity.java
+++ app/src/main/java/com/example/lenovobyeoz/fulicenter/activity/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.lenovobyeoz.fulicenter.R;
+import com.example.lenovobyeoz.fulicenter.utils.L;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        L.i("MainActivity onCreate");
     }
 }

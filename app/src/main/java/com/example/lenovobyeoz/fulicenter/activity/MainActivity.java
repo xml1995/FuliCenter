36d27a59c64d781a186c3b788f05980d4eb9f236
XueMeili <1604512498@qq.com>
2016-10-14 16:52:28 +0800

    修改项目图标，添加闪屏

--- app/src/main/java/com/example/lenovobyeoz/fulicenter/activity/MainActivity.java
+++ app/src/main/java/com/example/lenovobyeoz/fulicenter/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.lenovobyeoz.fulicenter;
+package com.example.lenovobyeoz.fulicenter.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.lenovobyeoz.fulicenter.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/example/lenovobyeoz/fulicenter/activity/SplashActivity.java
@@ -0,0 +1,38 @@
+package com.example.lenovobyeoz.fulicenter.activity;
+
+import android.content.Intent;
+import android.provider.Settings;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.lenovobyeoz.fulicenter.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static final long splashTime=2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+    @Override
+    protected  void onStart(){
+        super.onStart();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                long start= System.currentTimeMillis();
+                long costTime=System.currentTimeMillis() - start;
+                if(splashTime-costTime>0){
+                    try {
+                        Thread.sleep(splashTime-costTime);
+                    }catch (InterruptedException e){
+                        e.printStackTrace();
+                    }
+                }
+                startActivity(new Intent(SplashActivity.this,MainActivity.class));
+            }
+        }).start();
+    }
+}

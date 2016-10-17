8192a5570e51e79c5d826d2503e2ce8a31a3b940
XueMeili <1604512498@qq.com>
2016-10-17 09:15:23 +0800

    主页面底部菜单

--- app/src/main/java/com/example/lenovobyeoz/fulicenter/activity/SplashActivity.java
+++ app/src/main/java/com/example/lenovobyeoz/fulicenter/activity/SplashActivity.java
@@ -1,16 +1,10 @@
 package com.example.lenovobyeoz.fulicenter.activity;
-
-import android.content.Intent;
 import android.os.Handler;
-import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
 import com.example.lenovobyeoz.fulicenter.R;
 import com.example.lenovobyeoz.fulicenter.utils.MFGT;
-
 public class SplashActivity extends AppCompatActivity {
-
     private static final long sleepTime=2000;
 
     @Override

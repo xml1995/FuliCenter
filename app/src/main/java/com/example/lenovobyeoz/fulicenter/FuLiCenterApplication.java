ca254d519a11b8486cd591bd8d7eb726bb54de28
XueMeili <1604512498@qq.com>
2016-10-17 22:30:21 +0800

    添加网络请求的工具类以及application

---
+++ app/src/main/java/com/example/lenovobyeoz/fulicenter/FuLiCenterApplication.java
@@ -0,0 +1,20 @@
+package com.example.lenovobyeoz.fulicenter;
+
+import android.app.Application;
+
+/**
+ * Created by lenovoByEOZ on 2016/10/17.
+ */
+
+public class FuLiCenterApplication extends Application{
+    private static FuLiCenterApplication instance;
+    public FuLiCenterApplication(){
+        instance=this;
+    }
+    public static FuLiCenterApplication getInstance(){
+        if(instance==null){
+            instance=new FuLiCenterApplication();
+        }
+        return instance;
+    }
+}

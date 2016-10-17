3c61e9fb18e840461609914853f5c3bf01357573
XueMeili <1604512498@qq.com>
2016-10-17 17:14:32 +0800

    添加recyclerview的依赖

---
+++ app/src/main/java/com/example/lenovobyeoz/fulicenter/adapter/GoodsAdapter.java
@@ -0,0 +1,26 @@
+package com.example.lenovobyeoz.fulicenter.adapter;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.RecyclerView.Adapter;
+import android.support.v7.widget.RecyclerView.ViewHolder;
+import android.view.ViewGroup;
+
+/**
+ * Created by lenovoByEOZ on 2016/10/17.
+ */
+
+public class GoodsAdapter extends Adapter {
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
